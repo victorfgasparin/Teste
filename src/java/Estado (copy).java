37cb78cba8848e241e34f3b6b8bff68f3307a270
SergeMax <victor.maximillian@gmail.com>
2011-11-26 19:23:21 -0200

    testando commit de estrutura de diretorios dois

--- src/java/Estado (copy).java
+++ src/java/Estado (copy).java
@@ -14,25 +14,5 @@ public class Estado {
 	@GeneratedValue
 	private Long id;
 	private String nome;
-	@OneToMany(mappedBy="estado")
-	private List<Cidade> cidades;
 	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public List<Cidade> getCidades() {
-		return cidades;
-	}
-	public void setCidades(List<Cidade> cidades) {
-		this.cidades = cidades;
-	}
 }
