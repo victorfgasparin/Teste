6564dabbae59726c74e9c0a8e2779fdcdfb155e8
SergeMax <victor.maximillian@gmail.com>
2011-11-26 19:19:19 -0200

    testando commit de estrutura de diretorios

---
+++ src/java/Estado.java
@@ -0,0 +1,38 @@
+package br.com.fsi.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Estado {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String nome;
+	@OneToMany(mappedBy="estado")
+	private List<Cidade> cidades;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public List<Cidade> getCidades() {
+		return cidades;
+	}
+	public void setCidades(List<Cidade> cidades) {
+		this.cidades = cidades;
+	}
+}
